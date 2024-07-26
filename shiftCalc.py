fbdb87486efc4ac07b46fcb8ff478b90d679b9a1
Eddrick-23 <122147240+Eddrick-23@users.noreply.github.com>
2024-07-26 11:37:38 +0800

    Updates to include public holidays
    Calc_working_days method now gives info on number of public holidays missed due to working days and the specific public holiday names

--- shiftCalc.py
+++ shiftCalc.py
@@ -2,6 +2,8 @@ import pandas as pd
 from datetime import datetime
 from itertools import cycle
 from collections import defaultdict
+import holidays
+from tabulate import tabulate
 
 #define the calendar class which stores the shifts
 class Calendar():
@@ -69,14 +71,21 @@ class Calendar():
         calculate the number of working days from the "date" onwards.
         date input to be in 'YYYY-MM-DD' format
         '''
+        sg_holidays = holidays.country_holidays("SG") #to check public holidays for later
         day_data = defaultdict(int)
         working_days = 0
+        working_public_hols = defaultdict(int)
+        working_public_hols_name = defaultdict(list)
         non_working = 0
         for i in range(len(self.data.loc[pd.to_datetime(date):])):
+            d = self.data.index[i].date() #get the datetime obj
             day,shift = self.data.iloc[i]
-            if shift not in ["D1","D2"]:
+            if shift not in ["D1","D2"]: #if it is a working day
                 day_data[day] += 1
                 working_days += 1
+                if d in sg_holidays:
+                    working_public_hols[day] += 1
+                    working_public_hols_name[day].append(sg_holidays.get(d))
             else:
                 non_working += 1
         #dataframe consisting of working days for each date
@@ -85,9 +94,15 @@ class Calendar():
         df['Days'] = pd.Categorical(df['Days'], categories=custom_order, ordered=True)
         # Sort DataFrame based on 'Category' column
         sorted_df = df.sort_values(by='Days')
+        df_freqHols = pd.DataFrame(working_public_hols.items(),columns=["Days","Freq Holidays"])
+        df_holNames = pd.DataFrame(working_public_hols_name.items(), columns=["Days","Holiday(s)"])
+        sorted_df = sorted_df.merge(df_freqHols, on="Days", how = "left").fillna(0)
+        sorted_df = sorted_df.merge(df_holNames, on="Days", how = "left").fillna("None")
 
+        print(f"From {date} to {self.data.index[-1].date()}")
         print(f'Total:{working_days+non_working}, working:{working_days}, non working :{non_working}')
-        print(sorted_df.to_markdown(index=False))
+        
+        print(tabulate(sorted_df, tablefmt='grid', showindex=False))
             
 
 
