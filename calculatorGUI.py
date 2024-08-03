f81efb29861686991d61fad100c2d5d416673a94
Eddrick-23 <122147240+Eddrick-23@users.noreply.github.com>
2024-08-03 22:12:03 +0800

    updated user inputs
    Added calendar gui for user date inputs for ease of use

--- calculatorGUI.py
+++ calculatorGUI.py
@@ -2,26 +2,23 @@ import tkinter as tk
 from tkinter import messagebox
 from calculator import Calculator as Calc
 from tkinter import simpledialog
+from tkinter import ttk
+from tkcalendar import Calendar,DateEntry
 
 class calculatorGUI:
 
     def __init__(self):
         self.root = tk.Tk()
         self.root.geometry("800x500")
+        s = ttk.Style(self.root)
+        s.theme_use('clam')
 
         #button to get start date
-        label = tk.Label(self.root,text="Start Date", font=('Arial',18))
-        label.config(bg="green") # set label background or foreground color --> "fg = green"
-        label.pack(padx=10)
-        self.start_date = tk.Entry(self.root, font=("Arial",18))
-        self.start_date.pack()
+        ttk.Button(self.root, text="Start Date", command=lambda:self.getdate(setfor="start")).pack()
 
         #button to get end date
-        label = tk.Label(self.root,text="End Date", font=('Arial',18))
-        label.config(bg="green") # set label background or foreground color --> "fg = green"
-        label.pack(padx=10)
-        self.end_date = tk.Entry(self.root, font=("Arial",18))
-        self.end_date.pack()
+        ttk.Button(self.root, text="End Date", command=lambda:self.getdate(setfor="end")).pack()
+        
 
         #gets shifts
         label = tk.Label(self.root,text="Days In", font=('Arial',18))
@@ -42,7 +39,7 @@ class calculatorGUI:
         self.shift_names = tk.Entry(self.root, font=("Arial",18))
         self.shift_names.pack()
 
-        label = tk.Label(self.root,text="Shifts out of camp: e.g. out1,out2", font=('Arial',18))
+        label = tk.Label(self.root,text="Shifts out of ca20mp: e.g. out1,out2", font=('Arial',18))
         label.config(bg="green") # set label background or foreground color --> "fg = green"
         label.pack(padx=10)
         self.outcampshifts = tk.Entry(self.root, font=("Arial",18))
@@ -68,8 +65,8 @@ class calculatorGUI:
 
     def createCalc(self):
         #gets info from textboxes, creates calculator object
-        start_date = self.start_date.get()
-        end_date = self.end_date.get()
+        start_date = self.start_date
+        end_date = self.end_date
         days_in = self.days_in.get()
         days_out = self.days_out.get()
         shift_names = self.shift_names.get()
@@ -79,12 +76,31 @@ class calculatorGUI:
         self.calculator = Calc(start_date,end_date,days_in,days_out,shift_names,outcampshifts,shiftd1)
         print(messagebox.showinfo(title="Message", message="Calculator Created!"))
     
+    def getdate(self,setfor):
+
+        def set_sel(setfor):
+            if setfor == "start":
+                print("start date:" , cal.selection_get())
+                self.start_date = cal.selection_get()
+            elif setfor == "end":
+                print("end date:" , cal.selection_get())
+                self.end_date = cal.selection_get()
+            elif setfor == "user_input":
+                print("user input:", cal.selection_get())
+                self.user_input = cal.selection_get()
+
+                str1,str2,data = self.calculator.calc_working_days(self.user_input)
+                self.show_dataframe(str1,str2,data)
+
+        top = tk.Toplevel(self.root) #create separate window
+        cal = Calendar(top, font = ("Arial",14), selectmode = "day", cursor = "hand1", year = 2024, month = 1,day = 1)
+        cal.pack(fill = "both", expand = True)
+        ttk.Button(top, text = "ok", command=lambda:set_sel(setfor)).pack()
+        
+        
     def calc_wd(self):
         #show a pop up asking for a start date
-        user_input = simpledialog.askstring(title="Input",prompt="What is the start date?")
-        
-        str1,str2,data = self.calculator.calc_working_days(user_input)
-        self.show_dataframe(str1,str2,data)
+        self.getdate(setfor="user_input") 
     
     def show_dataframe(self, str1,str2,data):
         # Create a new window
@@ -92,7 +108,7 @@ class calculatorGUI:
         display_window.title(str1)
 
         # Create a Text widget to display the tabulated DataFrame
-        text_widget = tk.Text(display_window, wrap='none', font=20)
+        text_widget = tk.Text(display_window, wrap='none')
         text_widget.pack(expand=True, fill='both')
 
         # Insert the formatted string into the Text widget
@@ -100,17 +116,7 @@ class calculatorGUI:
         text_widget.insert('1.0', "\n")
         text_widget.insert('1.0', data) 
         text_widget.config(state='disabled')  # Make the Text widget read-only
-
-        #button to adjust text size
-        increaseButton = tk.Button(display_window,"+",command=self.increaseTextSize)
-        increaseButton.pack()
-        decreaseButton = tk.Button(display_window,"-",command=self.decreaseTextSize)
-        decreaseButton.pack()
     
-    def increaseTextSize(self):
-        pass
-    def decreaseTextSize(self):
-        pass
 
 
 calculatorGUI()
