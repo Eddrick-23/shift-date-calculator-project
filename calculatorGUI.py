47521ce11cfd8bf95d465712c15f902aca5ba6c1
Eddrick-23 <122147240+Eddrick-23@users.noreply.github.com>
2024-08-04 12:16:50 +0800

    update to input handling
    Date selection now displays the date selected on the GUI.
    Added error handling to user inputs before calculator is created.

--- calculatorGUI.py
+++ calculatorGUI.py
@@ -14,9 +14,13 @@ class calculatorGUI:
         s.theme_use('clam')
 
         #button to get start date
+        self.start_date_var = tk.StringVar(self.root, value="Not set")
+        tk.Label(self.root, textvariable=self.start_date_var).pack()
         ttk.Button(self.root, text="Start Date", command=lambda:self.getdate(setfor="start")).pack()
 
         #button to get end date
+        self.end_date_var = tk.StringVar(self.root, value = "Not set")
+        tk.Label(self.root, textvariable=self.end_date_var).pack()
         ttk.Button(self.root, text="End Date", command=lambda:self.getdate(setfor="end")).pack()
         
 
@@ -39,7 +43,7 @@ class calculatorGUI:
         self.shift_names = tk.Entry(self.root, font=("Arial",18))
         self.shift_names.pack()
 
-        label = tk.Label(self.root,text="Shifts out of ca20mp: e.g. out1,out2", font=('Arial',18))
+        label = tk.Label(self.root,text="Shifts out of camp: e.g. out1,out2", font=('Arial',18))
         label.config(bg="green") # set label background or foreground color --> "fg = green"
         label.pack(padx=10)
         self.outcampshifts = tk.Entry(self.root, font=("Arial",18))
@@ -65,13 +69,11 @@ class calculatorGUI:
 
     def createCalc(self):
         #gets info from textboxes, creates calculator object
-        start_date = self.start_date
-        end_date = self.end_date
-        days_in = self.days_in.get()
-        days_out = self.days_out.get()
-        shift_names = self.shift_names.get()
-        outcampshifts = self.outcampshifts.get()
-        shiftd1 = self.shiftd1.get()
+        
+        output = self.input_check()
+        if output == None:
+            return
+        start_date,end_date, days_in, days_out, shift_names, outcampshifts, shiftd1 = output
 
         self.calculator = Calc(start_date,end_date,days_in,days_out,shift_names,outcampshifts,shiftd1)
         print(messagebox.showinfo(title="Message", message="Calculator Created!"))
@@ -80,15 +82,18 @@ class calculatorGUI:
 
         def set_sel(setfor):
             if setfor == "start":
-                print("start date:" , cal.selection_get())
+                self.start_date_var.set(cal.selection_get())
                 self.start_date = cal.selection_get()
             elif setfor == "end":
-                print("end date:" , cal.selection_get())
+                self.end_date_var.set(cal.selection_get())
                 self.end_date = cal.selection_get()
             elif setfor == "user_input":
                 print("user input:", cal.selection_get())
                 self.user_input = cal.selection_get()
 
+                if self.user_input < self.start_date or self.user_input > self.end_date:
+                    messagebox.showerror(title="Error", message= f"{self.user_input} is out of range of \n start date: {self.start_date} \n end date: {self.end_date}")
+                    return
                 str1,str2,data = self.calculator.calc_working_days(self.user_input)
                 self.show_dataframe(str1,str2,data)
 
@@ -117,6 +122,76 @@ class calculatorGUI:
         text_widget.insert('1.0', data) 
         text_widget.config(state='disabled')  # Make the Text widget read-only
     
+    def input_check(self):
+        '''
+            Checks for valid user inputs. Prints an error message for invalid inputs
+        '''
+        try: 
+            start_date = self.start_date
+            end_date = self.end_date
+            days_in = self.days_in.get()
+            days_out = self.days_out.get()
+            shift_names = self.shift_names.get()
+            outcampshifts = self.outcampshifts.get()
+            shiftd1 = self.shiftd1.get()
+        except AttributeError:
+            messagebox.showerror(title="Error", message="Missing value field!")
+            return
+        attributelist = [days_in, days_out,shift_names, outcampshifts, shiftd1]
+        for a in attributelist:
+            if len(a) == 0:
+                messagebox.showerror(title="Error", message="Missing value field!")
+                return
+        
+        #check for valid start and end date
+        if self.start_date > self.end_date: 
+            messagebox.showerror(title="Error", message= "Start date must be before end date!")
+            return
+        
+        #check for valid days in and days out input
+        try:
+            din = int(self.days_in.get())
+            dout = int(self.days_out.get())
+        except ValueError:
+            messagebox.showerror(title="Error", message="days in and days out must be positive integers!")
+            return
+        if din <= 0 or dout <= 0:
+            messagebox.showerror(title="Error", message="days in and days out must be positive integers!")
+            return
+
+        #check for valid shift names and that number of shifts match days in and days out
+        
+        shiftnames = self.shift_names.get()
+        shiftnames = shift_names.replace(" ","") #remove whitespace
+        shiftnames = shiftnames.split(",")
+        final_shift_names = []
+        for s in shiftnames:
+            if s != "" and s not in final_shift_names:
+                final_shift_names.append(s)
+        target_length = din + dout
+        if len(final_shift_names) != target_length:
+            messagebox.showerror(title="Error", message=f"Length of shift names do not match cycle length. \n You have {len(final_shift_names)} shift names \n Your cycle length is {target_length} \n These are your current shifts {final_shift_names}" )
+            return
+        
+        #check for valid outshift days
+        outshifts = outcampshifts.replace(" ","")
+        outshifts = outshifts.split(",")
+        final_outshifts = []
+        for s in outshifts:
+            if s != "" and s not in final_outshifts:
+                final_outshifts.append(s)
+        if len(final_outshifts) != dout:
+            messagebox.showerror(title="Error", message=f"Your outshift days: {final_outshifts} do not match your days out: {dout}.")
+            return
+        if not final_outshifts <= final_shift_names:
+            messagebox.showerror(title="Error", message=f"Your outshift days: {final_outshifts} do not match your shift names: {final_shift_names}.")
+            return
+        
+        #check for valid shift on d1
+        if shiftd1 not in final_shift_names:
+            messagebox.showerror(title="Error", message=f"Your shift on day 1: {shiftd1} does not exist in your shift names: {final_shift_names}")
+            return
+        return start_date,end_date, days_in, days_out, shift_names, outcampshifts, shiftd1
 
 
 calculatorGUI()
